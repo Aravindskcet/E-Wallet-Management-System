efcd5ed110e291b4ffbec31c84224bb1eddbf3ac
Aravind <Aravind130906@gmail.com>
2026-06-26 21:55:01 +0530

    Issue #2: Setup React app with routing and project structure

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import api from "../services/api";
+
+console.log(api.defaults.baseURL);
+
+export default function Login() {
+  return <h1>Login Page</h1>;
+}
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import TopUp from "../pages/TopUp";
+import Transfer from "../pages/Transfer";
+import History from "../pages/History";
+import Admin from "../pages/Admin";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/topup" element={<TopUp />} />
+      <Route path="/transfer" element={<Transfer />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  );
+}
